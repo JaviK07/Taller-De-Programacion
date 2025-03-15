0c45bf6b3f240199d9ce8deda46f4e9f53dca4aa
Javier Kadjo <javiernaim@hotmail.com>
2025-03-15 17:43:03 -0300

    Actividad 7 - Inciso A - COMPLETO

--- Practica N1/questions.py
+++ Practica N1/questions.py
@@ -1,4 +1,5 @@
 import random
+import sys
 
 questions = [ # Preguntas para el juego
     "¿Que función se usa para obtener la longitud de una cadena en Python?",
@@ -32,13 +33,20 @@ for _ in range(3):
         print(f"{i + 1}. {answer}")
 
     for intento in range(2): # El usuario tiene 2 intentos para responder correctamente
-        user_answer = int(input("Respuesta: ")) - 1
+        try: # Cláusula TRY-EXCEPT utilizado para capturar errores y que el programa pueda seguir su ejecucion según se desee.
+            user_answer = int(input("Respuesta: ")) - 1
+        except ValueError:
+            print("Respuesta No Valida")
+            sys.exit(1) # Modulo incluido en Python para finalizar con un programa en ejecución.
 
         if user_answer == correct_answers_index[question_index]: # Se verifica si la respuesta es correcta
             print("¡Correcto!")
             break
-    else:
-        print("Incorrecto. La respuesta correcta es:") # Si el usuario no responde correctamente después de 2 intentos, se muestra la respuesta correcta
-        print(answers[question_index] [correct_answers_index[question_index]])
-
+        elif user_answer >= 4 or user_answer <= 0:
+            print('Respuesta No Valida')
+            sys.exit(1)
+        else:
+            print("Incorrecto. La respuesta correcta es:") # Si el usuario no responde correctamente después de 2 intentos, se muestra la respuesta correcta
+            print(answers[question_index] [correct_answers_index[question_index]])
+            
 print() # Se imprime un blanco al final de la pregunta
